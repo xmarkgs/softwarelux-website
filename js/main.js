7911ce3bd977a23da3f3c0a74d1a9fa02095c960
Marko Zhyvotkov <mraner123@gmail.com>
2021-10-24 16:41:00 +0300

    Functional updates
    First section fit to full screen; Header fixed on scroll; Service images change on hover;

--- js/main.js
+++ js/main.js
@@ -1,18 +1,21 @@
 /*
 
 File structure:
-1. Homepage
-2. Contact us modal
+1. For whom carousel
+2. Services change image
+3. Contact us modal
+4. Header change
 
 */
 
 
 $(document).ready(() => {
-    // [1] Homepage
 
-    // For whom carousel
+    // [1] For whom carousel
     $('#forWhomCarousel').slick({
         speed: 300,
+        autoplay: true,
+        autoplaySpeed: 10000,
         dots: true,
         prevArrow: `<button type="button" class="slick-prev"><svg width="16" height="26" viewBox="0 0 16 26" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path fill-rule="evenodd" clip-rule="evenodd" d="M10.4194 13L1.1628 3.74345C0.320733 2.90138 0.320733 1.53612 1.1628 0.694052C2.00487 -0.148016 3.37013 -0.148016 4.2122 0.694052L14.9934 11.4753C15.8355 12.3174 15.8355 13.6826 14.9934 14.5247L4.2122 25.3059C3.37013 26.148 2.00487 26.148 1.1628 25.3059C0.320733 24.4639 0.320733 23.0986 1.1628 22.2566L10.4194 13Z" fill="white"/>
@@ -24,7 +27,21 @@ $(document).ready(() => {
         </button>`,
     });
 
-    // [2] Contact us modal
+    // [2] Services change image
+    $('.services__item span').hover((event) => {
+        let services = document.querySelectorAll('.services__image');
+        let showService = event.target.dataset.id;
+
+        for (let image of services) {
+            if (image.dataset.id === showService) {
+                image.classList.add('active-service');
+            } else {
+                image.classList.remove('active-service');
+            }
+        }
+    });
+
+    // [3] Contact us modal
     $('.openContactModal').click(() => {
         $('body').addClass('no-scroll');
         $('.contact-us-modal').addClass('active');
@@ -41,5 +58,14 @@ $(document).ready(() => {
         $('.contact-us-modal__content').click((event) => {
             event.stopPropagation();
         });
+    });
+
+    // [4] Header change
+    $(document).scroll(() => {
+        if ($(document).scrollTop() > 50) {
+            $('header').removeClass('header--top');
+        } else {
+            $('header').addClass('header--top');
+        }
     });
 });
